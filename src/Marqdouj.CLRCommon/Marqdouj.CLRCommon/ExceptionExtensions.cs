ca12935bba6fd0677e63acde2ce79ce347bcd9ac
marqdouj <16510416+marqdouj@users.noreply.github.com>
2025-02-17 14:52:51 -0700

    v8.1.1 Added MinMaxN INumber

---
+++ src/Marqdouj.CLRCommon/Marqdouj.CLRCommon/ExceptionExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Marqdouj.CLRCommon
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Resolves all messages, including inner exceptions.
+        /// Originally designed for use with Aggregate exceptions, but will work with any Exception.
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static string ToMessage(this Exception ex)
+        {
+            var messages = ex.ToMessageList();
+            return string.Join("\n", messages);
+        }
+
+        /// <summary>
+        /// <see cref="ToMessage(Exception)"/>
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static List<string> ToMessageList(this Exception ex)
+        {
+            var messages = new List<string>();
+            AddMessagesToList(messages, ex);
+            return messages;
+        }
+
+        private static void AddMessagesToList(List<string> messages, Exception? ex)
+        {
+            if (ex == null) return;
+
+            if (ex is AggregateException aggregate)
+            {
+                foreach (var item in aggregate.InnerExceptions)
+                {
+                    if (item is not AggregateException)
+                        AddMessageToList(messages, item.Message);
+
+                    AddMessagesToList(messages, item.InnerException);
+                }
+            }
+            else
+            {
+                AddMessageToList(messages, ex.Message);
+                AddMessagesToList(messages, ex.InnerException);
+            }
+        }
+
+        private static void AddMessageToList(List<string> messages, string message)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+                messages.Add(message);
+        }
+    }
+}
---
+++ src/Marqdouj.CLRCommon/Marqdouj.CLRCommon/MinMaxN.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace Marqdouj.CLRCommon
+{
+    public class MinMaxN<T> : StateContainer where T : INumber<T>
+    {
+        private T _value;
+
+        public MinMaxN(T min, T max, T value)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(min, max);
+
+            Min = min;
+            Max = max;
+            _value = value;
+        }
+
+        public T Min { get; }
+
+        public T Max { get; }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (value < Min)
+                    value = Min;
+                if (value > Max)
+                    value = Max;
+
+                SetValue(ref _value, value);
+            }
+        }
+    }
+}
